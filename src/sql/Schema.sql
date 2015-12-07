d2ed588e95167982493585339dd8ebf432cb04d4
Jeremy Friesen <rejemy@yahoo.com>
2015-12-07 11:13:56 -0800

    Adding MySQL schema creation script

---
+++ src/sql/Schema.sql
@@ -0,0 +1,197 @@
+CREATE DATABASE  IF NOT EXISTS `serverville` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `serverville`;
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
+--
+-- Host: 127.0.0.1    Database: serverville
+-- ------------------------------------------------------
+-- Server version	5.6.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin_log`
+--
+
+DROP TABLE IF EXISTS `admin_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin_log` (
+  `requestid` varchar(255) NOT NULL,
+  `userid` varchar(255) NOT NULL,
+  `created` bigint(20) NOT NULL,
+  `connectionid` varchar(255) NOT NULL,
+  `api` varchar(255) NOT NULL,
+  `request` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`requestid`),
+  KEY `UserIndex` (`userid`),
+  KEY `CreatedIndex` (`created`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `adminsession`
+--
+
+DROP TABLE IF EXISTS `adminsession`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adminsession` (
+  `id` varchar(255) NOT NULL,
+  `userid` varchar(255) NOT NULL,
+  `started` bigint(20) NOT NULL,
+  `lastactive` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `adminsession_userid`
+--
+
+DROP TABLE IF EXISTS `adminsession_userid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adminsession_userid` (
+  `userid` varchar(255) NOT NULL,
+  `id` varchar(255) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `agent_key`
+--
+
+DROP TABLE IF EXISTS `agent_key`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `agent_key` (
+  `key` varchar(255) NOT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `iprange` varchar(45) DEFAULT NULL,
+  `expiration` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `keydata_item`
+--
+
+DROP TABLE IF EXISTS `keydata_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `keydata_item` (
+  `id` varchar(255) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `data` varbinary(62000) DEFAULT NULL,
+  `datatype` int(11) NOT NULL,
+  `created` bigint(20) NOT NULL,
+  `modified` bigint(20) NOT NULL,
+  `visibility` tinyint(4) NOT NULL,
+  `deleted` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`,`key`),
+  KEY `ModifiedTime` (`modified`),
+  KEY `DeletedIndex` (`deleted`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `script`
+--
+
+DROP TABLE IF EXISTS `script`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `script` (
+  `id` varchar(255) NOT NULL,
+  `source` longtext NOT NULL,
+  `created` bigint(20) NOT NULL,
+  `modified` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` varchar(255) NOT NULL,
+  `sessionid` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `passwdhash` varchar(255) DEFAULT NULL,
+  `created` bigint(20) NOT NULL,
+  `modified` bigint(20) NOT NULL,
+  `admin` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_email`
+--
+
+DROP TABLE IF EXISTS `user_email`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_email` (
+  `email` varchar(255) NOT NULL,
+  `id` varchar(255) DEFAULT NULL,
+  `hold` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_sessionid`
+--
+
+DROP TABLE IF EXISTS `user_sessionid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_sessionid` (
+  `sessionid` varchar(255) NOT NULL,
+  `id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`sessionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_username`
+--
+
+DROP TABLE IF EXISTS `user_username`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_username` (
+  `username` varchar(255) NOT NULL,
+  `id` varchar(255) DEFAULT NULL,
+  `hold` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-07 11:12:16
